4978dc422bd03e571d54ddf77b4a1221a8aa4996
docktermj <michael@dockter.com>
2022-02-14 22:27:07 -0600

    #1 Savepoint

--- xyzzy.go
+++ xyzzy.go
@@ -1,16 +1,48 @@
 package xyzzy
 
+/*
+#cgo CFLAGS: -g -Wall
+#include <stdlib.h>
+#include <stdio.h>
+#include "g2PluginInterface.h"
+#include "libg2.h"
+#include "libg2config.h"
+#include "libg2configmgr.h"
+#include "libg2diagnostic.h"
+#include "libg2hasher.h"
+#include "libg2product.h"
+#include "libg2ssadm.h"
+*/
+import "C"
 import (
 	"fmt"
 )
 
 // Values updated via "go install -ldflags" parameters.
 
-var moduleName string = "github.com/docktermj/go-hello-world-module"
+var moduleName string = "github.com/docktermj/go-xyzzy-sdk"
 var buildVersion string = "0.0.5"
 var buildIteration string = "0"
-var helloName string = "world"
 
-func Hello() string {
-	return fmt.Sprintf("Hello, %s! from %s version %s-%s", helloName, moduleName, buildVersion, buildIteration)
+// ----------------------------------------------------------------------------
+// libg2diagnostic.h
+// ----------------------------------------------------------------------------
+
+func G2Diagnostic_getPhysicalCores() int {
+    result := C.G2Diagnostic_getPhysicalCores()
+    return result
+}
+
+// ----------------------------------------------------------------------------
+// main
+// ----------------------------------------------------------------------------
+
+
+func main() {
+
+	physicalCores := G2Diagnostic_getPhysicalCores()
+
+	fmt.Println("Physical cores %d", physicalCores)
+
+	regreet()
 }
